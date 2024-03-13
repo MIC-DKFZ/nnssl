1d8063b619739ef649c4839e21a3bf745ef487bb
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-03-13 17:52:32 +0100

    Fix local conditional import

--- nnssl/apitoken.py
+++ nnssl/apitoken.py
@@ -6,5 +6,6 @@ def get_valohai_api_token():
     if is_running_in_valohai():
         return os.environ["VALOHAI_API_TOKEN"]
     else:
-        import secret_token
-        return secret_token.valohai_api_token
+        from nnssl.secret_token import valohai_api_token
+
+        return valohai_api_token
