d5423d5cd2cb13705caea4ba27ca7252797dd393
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-04-16 09:12:31 +0200

    Add try except wrapping to save latest checkpoint on keyboard interrupt.

--- nnssl/training/nnsslTrainer/AbstractTrainer.py
+++ nnssl/training/nnsslTrainer/AbstractTrainer.py
@@ -231,27 +231,32 @@ class AbstractBaseTrainer(ABC):
             )
 
     def run_training(self):
-        self.on_train_start()
+        try:
+            self.on_train_start()
 
-        for epoch in range(self.current_epoch, self.num_epochs):
-            self.on_epoch_start()
+            for epoch in range(self.current_epoch, self.num_epochs):
+                self.on_epoch_start()
 
-            self.on_train_epoch_start()
-            train_outputs = []
-            for batch_id in range(self.num_iterations_per_epoch):
-                train_outputs.append(self.train_step(next(self.dataloader_train)))
-            self.on_train_epoch_end(train_outputs)
+                self.on_train_epoch_start()
+                train_outputs = []
+                for batch_id in range(self.num_iterations_per_epoch):
+                    train_outputs.append(self.train_step(next(self.dataloader_train)))
+                self.on_train_epoch_end(train_outputs)
 
-            with torch.no_grad():
-                self.on_validation_epoch_start()
-                val_outputs = []
-                for batch_id in range(self.num_val_iterations_per_epoch):
-                    val_outputs.append(self.validation_step(next(self.dataloader_val)))
-                self.on_validation_epoch_end(val_outputs)
+                with torch.no_grad():
+                    self.on_validation_epoch_start()
+                    val_outputs = []
+                    for batch_id in range(self.num_val_iterations_per_epoch):
+                        val_outputs.append(self.validation_step(next(self.dataloader_val)))
+                    self.on_validation_epoch_end(val_outputs)
 
-            self.on_epoch_end()
+                self.on_epoch_end()
 
-        self.on_train_end()
+            self.on_train_end()
+        except KeyboardInterrupt:
+            self.print_to_log_file("Keyboard interrupt. Exiting gracefully.")
+            self.save_checkpoint(join(self.output_folder, "checkpoint_latest.pth"))
+            raise KeyboardInterrupt
 
     def print_to_log_file(self, *args, also_print_to_console=True, add_timestamp=True):
         if self.local_rank == 0:
