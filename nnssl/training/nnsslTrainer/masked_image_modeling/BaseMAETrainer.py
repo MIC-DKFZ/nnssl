9cf704777aefd931b1355a16d2a4f2a9a3b8f9dc
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-08-23 08:34:56 +0200

    Add graceful exiting to BaseMAE classes

--- nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
+++ nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
@@ -365,37 +365,42 @@ class BaseMAETrainer(AbstractBaseTrainer):
         return dl_val
 
     def run_training(self):
-        self.on_train_start()
-        if self.local_rank == 0:
-            self.log_qualitative_reconstruction_step()  # Do a quick test everything works.
-        for epoch in range(self.current_epoch, self.num_epochs):
-            self.on_epoch_start()
+        try:
+            self.on_train_start()
+            if self.local_rank == 0:
+                self.log_qualitative_reconstruction_step()  # Do a quick test everything works.
+            for epoch in range(self.current_epoch, self.num_epochs):
+                self.on_epoch_start()
 
-            self.on_train_epoch_start()
-            train_outputs = []
-            for batch_id in range(self.num_iterations_per_epoch):
-                train_outputs.append(self.train_step(next(self.dataloader_train)))
-            self.on_train_epoch_end(train_outputs)
+                self.on_train_epoch_start()
+                train_outputs = []
+                for batch_id in range(self.num_iterations_per_epoch):
+                    train_outputs.append(self.train_step(next(self.dataloader_train)))
+                self.on_train_epoch_end(train_outputs)
 
-            with torch.no_grad():
-                self.on_validation_epoch_start()
-                val_outputs = []
-                for batch_id in range(self.num_val_iterations_per_epoch):
-                    val_batch = next(self.dataloader_val)
-                    val_outputs.append(self.validation_step(val_batch))
-                self.on_validation_epoch_end(val_outputs)
+                with torch.no_grad():
+                    self.on_validation_epoch_start()
+                    val_outputs = []
+                    for batch_id in range(self.num_val_iterations_per_epoch):
+                        val_batch = next(self.dataloader_val)
+                        val_outputs.append(self.validation_step(val_batch))
+                    self.on_validation_epoch_end(val_outputs)
 
-                # ------------------------ Maybe Log qualitative recon ----------------------- #
-                if (self.current_epoch + 1) % self.save_imgs_every_n_epochs == 0:
-                    if self.local_rank == 0:
-                        self.log_qualitative_reconstruction_step()
-                        # self.save_checkpoint(
-                        #     join(self.output_folder, f"checkpoint_epoch_{self.current_epoch}.pth"), live_upload=True
-                        # )
+                    # ------------------------ Maybe Log qualitative recon ----------------------- #
+                    if (self.current_epoch + 1) % self.save_imgs_every_n_epochs == 0:
+                        if self.local_rank == 0:
+                            self.log_qualitative_reconstruction_step()
+                            # self.save_checkpoint(
+                            #     join(self.output_folder, f"checkpoint_epoch_{self.current_epoch}.pth"), live_upload=True
+                            # )
 
-            self.on_epoch_end()
+                self.on_epoch_end()
 
-        self.on_train_end()
+            self.on_train_end()
+        except KeyboardInterrupt:
+            self.print_to_log_file("Keyboard interrupt. Exiting gracefully.")
+            self.save_checkpoint(join(self.output_folder, "checkpoint_latest.pth"))
+            raise KeyboardInterrupt
 
     @staticmethod
     def get_training_transforms(
