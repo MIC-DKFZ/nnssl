acf18ce82a933d3699ccfe9e37ce47cd9eaf5026
GregorKoehler <gregor.koe@googlemail.com>
2024-09-04 20:51:48 +0200

    add simclr transform and account for nnUNet differences...

--- nnssl/ssl_data/dataloading/simclr_transform.py
+++ nnssl/ssl_data/dataloading/simclr_transform.py
@@ -1,4 +1,6 @@
+import torch
 from batchgenerators.transforms.abstract_transforms import AbstractTransform
+from batchgenerators.transforms.utility_transforms import RenameTransform
 
 
 class SimCLRTransform(AbstractTransform):
@@ -9,9 +11,12 @@ class SimCLRTransform(AbstractTransform):
         return tuple of transformed data_dicts
         """
         self.transforms = transforms
+        self.rename = RenameTransform(in_key="data", out_key="image", delete_old=True)
 
     def __call__(self, **data_dict):
-        xi = self.transforms(**data_dict)
-        xj = self.transforms(**data_dict)
+        renamed = self.rename(**data_dict)
+        renamed["image"] = torch.from_numpy(renamed["image"]).squeeze().float()
+        xi = self.transforms(**renamed)
+        xj = self.transforms(**renamed)
 
-        return xi, xj
+        return {"image_i": xi["image"], "image_j": xj["image"]}
