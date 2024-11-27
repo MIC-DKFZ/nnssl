b86a260e60bf95d1de4d06da99242723cd1074c8
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-11-27 14:47:09 +0100

    Add Dataset743 ABCD_NIH creation

--- src/nnssl/dataset_conversion/Dataset743_ABCD_NIH.py
+++ src/nnssl/dataset_conversion/Dataset743_ABCD_NIH.py
@@ -4,6 +4,9 @@ from pathlib import Path
 from tqdm import tqdm
 import shutil
 from multiprocessing import Pool
+from batchgenerators.utilities.file_and_folder_operations import join, maybe_mkdir_p, isdir, load_json, save_json
+
+from nnssl.data.raw_dataset import Collection, Dataset, Image, Subject, Session
 
 path_to_abcd = Path("/mnt/E132-Rohdaten/wald_collection/ABCD_NIH")
 
@@ -39,21 +42,44 @@ def copy_over_files(source_path, target_path):
         shutil.copy(source_path, target_path)
 
 
-def move_to_cluster():
+def create_collection_json():
     target_path = path_to_abcd / Path("abcd_bids")
-    cluster_target_path = Path("/mnt/cluster-data-all/t006d/nnunetv2/nnssl_raw")
+    dataset_name = "Dataset743_ABCD_NIH"
+    cluster_target_path = Path(f"/mnt/cluster-data-all/t006d/nnunetv2/nnssl_raw/{dataset_name}")
     cluster_target_path.mkdir(exist_ok=True, parents=True)
     all_nifti_paths = get_nii_files_in_subtree(target_path)
-    source_target_pairs = [
-        (str(source_path), str(cluster_target_path / (Path(source_path).name))) for source_path in all_nifti_paths
-    ]
-    print("Moving to cluster")
-    with Pool(24) as pool:
-        pool.starmap(copy_over_files, source_target_pairs)
-    # for src_path, tgt_path in tqdm(source_target_pairs):
-    #     copy_over_files(src_path, tgt_path)
+
+    collection: Collection
+    collection = Collection(
+        collection_index=743,
+        collection_name="Dataset743_ABCD_NIH",
+    )
+    dataset: Dataset
+    dataset = Dataset(dataset_index=743, name="ABCD_NIH", dataset_info=None)
+    collection.datasets["abcd"] = dataset
+
+    for nifti_path in tqdm(all_nifti_paths, desc="Inserting files into collection."):
+        parents = str(nifti_path).split("/")
+        subject_id = parents[-4]
+        session_id = parents[-3]
+        image_name = parents[-1]
+        modality = "T1" if "T1w" in image_name else "T2"
+
+        if subject_id not in dataset.subjects:
+            subj = Subject(subject_id=subject_id, subject_info=None)
+            dataset.subjects[subject_id] = subj
+        subj = dataset.subjects[subject_id]
+        if session_id not in subj.sessions:
+            sess = Session(session_id=session_id)
+            subj.sessions[session_id] = sess
+        sess = subj.sessions[session_id]
+
+        img = Image(name=image_name, image_path=nifti_path, modality=modality, image_info=None, associated_masks=None)
+        sess.images.append(img)
+    collection_dict = collection.to_dict(relative_paths=True)
+    save_json(collection_dict, os.path.join(cluster_target_path, "pretrain_data.json"))
 
 
 if __name__ == "__main__":
     # main()
-    move_to_cluster()
+    create_collection_json()
