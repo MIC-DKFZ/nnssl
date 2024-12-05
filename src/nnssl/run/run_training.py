d57b03a3fab55fdd659ed19179ecf2a3e7a3687a
d246a <d.trofimova@dkfz-heidelberg.de>
2024-12-05 10:42:28 +0100

    removing device from trainer init

--- src/nnssl/run/run_training.py
+++ src/nnssl/run/run_training.py
@@ -208,7 +208,7 @@ def run_ddp(
     #torch.cuda.set_device(torch.device("cuda", dist.get_rank()))
  
     device = torch.device(f"cuda:{rank}")
-
+    print(device)
     nnunet_trainer = get_trainer_from_args(dataset_name_or_id, configuration, fold, tr, p, device=device, *args, **kwargs)
 
     if disable_checkpointing:
