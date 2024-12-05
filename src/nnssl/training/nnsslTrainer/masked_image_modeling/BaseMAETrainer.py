d4d73902e5ed5baa60e02bae2de10257fe864f02
d246a <d.trofimova@dkfz-heidelberg.de>
2024-12-05 13:03:49 +0100

    working for single gpu

--- src/nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
+++ src/nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
@@ -64,10 +64,27 @@ class BaseMAETrainer(AbstractBaseTrainer):
         configuration_name: str,
         fold: int,
         pretrain_json: dict,
-        device: torch.device = torch.device("cuda"),
+        device: torch.device,
+        mask_ratio,
+        vit_patch_size,
+        embed_dim,
+        encoder_eva_depth,
+        encoder_eva_numheads,
+        decoder_eva_depth,
+        decoder_eva_numheads,
+        bs,
+        
+
     ):
         # plan.configurations[configuration_name].batch_size = 1
-        super().__init__(plan, configuration_name, fold, pretrain_json, device)
+        super().__init__(plan, configuration_name, fold, pretrain_json, device, mask_ratio,
+                vit_patch_size,
+                embed_dim,
+                encoder_eva_depth,
+                encoder_eva_numheads,
+                decoder_eva_depth,
+                decoder_eva_numheads,
+                bs,)
         self.mask_percentage: float = 0.75
 
         self.im_output_folder = os.path.join(self.output_folder, "img_log")
