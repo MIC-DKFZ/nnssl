a4ffce2402c82a2a144e950f86c5afd9b72867d1
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-12-25 20:09:52 +0100

    Add larger and smaller patch sizes

---
+++ src/nnssl/training/nnsslTrainer/masked_image_modeling/base_mae_derivates/var_patch_sizes.py
@@ -0,0 +1,47 @@
+import os
+import torch
+from nnssl.experiment_planning.experiment_planners.plan import Plan
+from nnssl.training.nnsslTrainer.masked_image_modeling.BaseMAETrainer import BaseMAETrainer
+import numpy as np
+
+
+class BaseMAETrainer_BS8_ep1000_patch_128(BaseMAETrainer):
+
+    def __init__(
+        self,
+        plan: Plan,
+        configuration_name: str,
+        fold: int,
+        pretrain_json: dict,
+        device: torch.device = torch.device("cuda"),
+    ):
+        plan.configurations[configuration_name].patch_size = np.array([128, 128, 128])
+        super().__init__(plan, configuration_name, fold, pretrain_json, device)
+
+
+class BaseMAETrainer_BS8_ep1000_patch_160(BaseMAETrainer):
+
+    def __init__(
+        self,
+        plan: Plan,
+        configuration_name: str,
+        fold: int,
+        pretrain_json: dict,
+        device: torch.device = torch.device("cuda"),
+    ):
+        plan.configurations[configuration_name].patch_size = np.array([160, 160, 160])
+        super().__init__(plan, configuration_name, fold, pretrain_json, device)
+
+
+class BaseMAETrainer_BS8_ep1000_patch_224(BaseMAETrainer):
+
+    def __init__(
+        self,
+        plan: Plan,
+        configuration_name: str,
+        fold: int,
+        pretrain_json: dict,
+        device: torch.device = torch.device("cuda"),
+    ):
+        plan.configurations[configuration_name].patch_size = np.array([224, 224, 224])
+        super().__init__(plan, configuration_name, fold, pretrain_json, device)
