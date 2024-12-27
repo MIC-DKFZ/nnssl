8b9d4691dfcfd5fc57b1b4cd5c5849a21dabd31e
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-12-27 09:06:12 +0100

    Add missing loss funtions

--- src/nnssl/training/loss/mse_loss.py
+++ src/nnssl/training/loss/mse_loss.py
@@ -1,6 +1,8 @@
 from torch import nn
 import torch
 from nnssl.training.loss.abstract_loss import AbstractLoss
+from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+from einops import rearrange
 
 
 class MAEMSELoss(AbstractLoss):
@@ -17,3 +19,72 @@ class MAEMSELoss(AbstractLoss):
         reconstruction_loss = torch.sum(reconstruction_loss * (1 - mask)) / torch.sum((1 - mask))
 
         return reconstruction_loss
+
+
+class MAEL1Loss(AbstractLoss):
+    def __init__(self):
+        super().__init__()
+        self.loss = nn.L1Loss(reduction="none")
+
+    def forward(
+        self, model_output: torch.Tensor, target: dict[str, torch.Tensor], mask: torch.Tensor
+    ) -> torch.Tensor:
+        """Can take any outputs,  ."""
+        # Mask = 1 represents not masked points
+        reconstruction_loss = torch.sum(torch.abs(model_output - target) * (1 - mask)) / torch.sum((1 - mask))
+
+        return reconstruction_loss
+
+
+class MAESSIMLoss(AbstractLoss):
+    def __init__(self):
+        super().__init__()
+        self.loss = nn.MSELoss(reduction="none")
+
+    def forward(
+        self, model_output: torch.Tensor, target: dict[str, torch.Tensor], mask: torch.Tensor
+    ) -> torch.Tensor:
+        """Can take any outputs,  ."""
+        # Mask = 1 represents not masked points
+        # Rescale output and target to [0, 1] for each image in batch
+        output_min = torch.amin(model_output, dim=(1, 2, 3, 4), keepdim=True)
+        output_max = torch.amax(model_output, dim=(1, 2, 3, 4), keepdim=True)
+        target_min = torch.amin(target, dim=(1, 2, 3, 4), keepdim=True)
+        target_max = torch.amax(target, dim=(1, 2, 3, 4), keepdim=True)
+
+        rescaled_out = (model_output - output_min) / (output_max - output_min)
+        rescaled_target = (target - target_min) / (target_max - target_min)
+
+        # rescaled_out = rearrange(rescaled_out, "b x y z c -> b c x y z")
+        # rescaled_target = rearrange(rescaled_target, "b x y z c -> b c x y z")
+
+        ssim_loss = 1 - ssim(rescaled_out, rescaled_target, data_range=1, size_average=False, nonnegative_ssim=True)
+
+        return ssim_loss
+
+
+class MAE_MS_SSIMLoss(AbstractLoss):
+    def __init__(self):
+        super().__init__()
+        self.loss = nn.MSELoss(reduction="none")
+
+    def forward(
+        self, model_output: torch.Tensor, target: dict[str, torch.Tensor], mask: torch.Tensor
+    ) -> torch.Tensor:
+        """Can take any outputs,  ."""
+        # Mask = 1 represents not masked points
+        # Rescale output and target to [0, 1] for each image in batch
+        output_min = torch.amin(model_output, dim=(1, 2, 3, 4), keepdim=True)
+        output_max = torch.amax(model_output, dim=(1, 2, 3, 4), keepdim=True)
+        target_min = torch.amin(target, dim=(1, 2, 3, 4), keepdim=True)
+        target_max = torch.amax(target, dim=(1, 2, 3, 4), keepdim=True)
+
+        rescaled_out = (model_output - output_min) / (output_max - output_min)
+        rescaled_target = (target - target_min) / (target_max - target_min)
+
+        # rescaled_out = rearrange(rescaled_out, "b x y z c -> b c x y z")
+        # rescaled_target = rearrange(rescaled_target, "b x y z c -> b c x y z")
+
+        ssim_loss = 1 - ms_ssim(rescaled_out, rescaled_target, data_range=1, size_average=False, win_size=7)
+
+        return ssim_loss
