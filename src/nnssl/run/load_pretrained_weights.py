6f7b64e381d4679170daa67c25eb4abc83e39aa2
Tenbatsu24 <puruvaish24@outlook.com>
2025-06-16 13:47:42 +0200

    change: ignore if key missing in pretrained dict since we might be adapting from another strategy

--- src/nnssl/run/load_pretrained_weights.py
+++ src/nnssl/run/load_pretrained_weights.py
@@ -1,4 +1,7 @@
+import warnings
+
 import torch
+
 from torch._dynamo import OptimizedModule
 from torch.nn.parallel import DistributedDataParallel as DDP
 
@@ -32,14 +35,20 @@ def load_pretrained_weights(network, fname, verbose=False):
     model_dict = mod.state_dict()
     # verify that all but the segmentation layers have the same shape
     for key, _ in model_dict.items():
-        if all([i not in key for i in skip_strings_in_pretrained]):
-            assert key in pretrained_dict, \
-                f"Key {key} is missing in the pretrained model weights. The pretrained weights do not seem to be " \
-                f"compatible with your network."
-            assert model_dict[key].shape == pretrained_dict[key].shape, \
-                f"The shape of the parameters of key {key} is not the same. Pretrained model: " \
-                f"{pretrained_dict[key].shape}; your network: {model_dict[key]}. The pretrained model " \
-                f"does not seem to be compatible with your network."
+        if all([i not in key for i in skip_strings_in_pretrained]):  # perform check only for non-segmentation layers
+            if key not in pretrained_dict:
+                # user warning
+                warnings.warn(
+                    f"{'=' * 20} WARNING {'=' * 20}\n"
+                    f"Key {key} is missing in the pretrained model weights. The pretrained weights do not seem to be "
+                    "compatible with your network. This is not an error, but you should check whether this is intended."
+                    f"\n{'=' * 20} WARNING {'=' * 20}"
+                )
+            if (key in model_dict) and (key in pretrained_dict):
+                assert model_dict[key].shape == pretrained_dict[key].shape, \
+                    f"The shape of the parameters of key {key} is not the same. Pretrained model: " \
+                    f"{pretrained_dict[key].shape}; your network: {model_dict[key]}. The pretrained model " \
+                    f"does not seem to be compatible with your network."
 
     # fun fact: in principle this allows loading from parameters that do not cover the entire network. For example pretrained
     # encoders. Not supported by this function though (see assertions above)
@@ -50,8 +59,11 @@ def load_pretrained_weights(network, fname, verbose=False):
     #                    if (('module.' + k if is_ddp else k) in model_dict) and
     #                    all([i not in k for i in skip_strings_in_pretrained])}
 
-    pretrained_dict = {k: v for k, v in pretrained_dict.items()
-                       if k in model_dict.keys() and all([i not in k for i in skip_strings_in_pretrained])}
+    pretrained_dict = {
+        k: v
+        for k, v in pretrained_dict.items()
+        if k in model_dict.keys() and all([i not in k for i in skip_strings_in_pretrained])
+    }
 
     model_dict.update(pretrained_dict)
 
@@ -62,5 +74,3 @@ def load_pretrained_weights(network, fname, verbose=False):
             print(key, 'shape', value.shape)
         print("################### Done ###################")
     mod.load_state_dict(model_dict)
-
-
